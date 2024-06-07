de3bfc092c549c68865586163ca8588a8178f306
Jesusborjas006 <jesusbor006@gmail.com>
2024-06-06 19:00:30 -0500

    Displays random poster on page load and create btn to load different poster

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,30 @@
+import { useState } from "react";
 import { images, titles, quotes } from "../data";
 
 const getRandomNumber = (arr: string[]) => {
-  return Math.floor(Math.random() * arr.length) + 1;
+  return Math.floor(Math.random() * arr.length);
 };
 
 const Home = () => {
+  const [imageIndex, setImageIndex] = useState(getRandomNumber(images));
+  const [titlesIndex, setTitlesIndex] = useState(getRandomNumber(titles));
+  const [quotesIndex, setQuotesIndex] = useState(getRandomNumber(quotes));
+
+  const handleRandomPoster = () => {
+    setImageIndex(getRandomNumber(images));
+    setTitlesIndex(getRandomNumber(titles));
+    setQuotesIndex(getRandomNumber(quotes));
+  };
+
   return (
     <main>
-      <img
-        src={"src/" + images[getRandomNumber(images)]}
-        alt="Random Picture"
-      />
+      <img src={"src/" + images[imageIndex]} alt="Random Picture" />
       <h1 className="text-4xl font-semibold font-serif">
-        {titles[getRandomNumber(titles)]}
+        {titles[titlesIndex]}
       </h1>
-      <p>{quotes[getRandomNumber(quotes)]}</p>
+      <p>{quotes[quotesIndex]}</p>
+
+      <button onClick={handleRandomPoster}>Show Random Poster</button>
     </main>
   );
 };
