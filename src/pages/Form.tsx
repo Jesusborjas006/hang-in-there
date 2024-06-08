2723b96e819162db78d29927264d2d86436348fc
Jesusborjas006 <jesusbor006@gmail.com>
2024-06-07 20:29:06 -0500

    Create two files for pages and install react router

---
+++ src/pages/Form.tsx
@@ -0,0 +1,5 @@
+const Form = () => {
+  return <h2>Form Page</h2>;
+};
+
+export default Form;
---
+++ src/pages/SavedPosters.tsx
@@ -0,0 +1,5 @@
+const SavedPosters = () => {
+  return <h2>Saved Posters Page</h2>;
+};
+
+export default SavedPosters;
